8718834ae4e399436b01913e84635e324cb8e885
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2020-12-09 19:23:36 +0530

    Implemented custom scope & setting defaults

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,51 +1,52 @@
 class PortfoliosController < ApplicationController
-    def index
-         @portfolio_items = Portfolio.all
-    end
+  def index
+    @portfolio_items = Portfolio.all
+    @angular_items = Portfolio.angular
+    @ruby_items = Portfolio.ruby_on_rails_portfolio_items
+  end
 
-    def new
-        @portfolio_item = Portfolio.new
-    end
-    
-    def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-    
-        respond_to do |format|
-          if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-          else
-            format.html { render :new }
-          end
-        end
-    end
+  def new
+    @portfolio_item = Portfolio.new
+  end
 
-    def edit
-        @portfolio_item = Portfolio.find(params[:id])
-    end
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-    
-    def update
-        @portfolio_item = Portfolio.find(params[:id])
-        respond_to do |format|
-            if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-                format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-            else
-                format.html { render :edit }
-            end
-        end
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live." }
+      else
+        format.html { render :new }
+      end
     end
+  end
 
-    def show
-        @portfolio_item = Portfolio.find(params[:id])
-    end
-    
-    def destroy
-        @portfolio_item = Portfolio.find(params[:id])
-        
-        @portfolio_item.destroy
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-        respond_to do |format|
-          format.html { redirect_to portfolios_url, notice: 'Record was successfully destroyed.' }
-        end
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: "The record was successfully updated." }
+      else
+        format.html { render :edit }
+      end
     end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Record was successfully destroyed." }
+    end
+  end
 end
