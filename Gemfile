a52285af8cdbc48107282537c912e6a1b73fdba2
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2020-12-14 16:39:23 +0530

    Integrated new gem for copyright

--- Gemfile
+++ Gemfile
@@ -52,3 +52,5 @@ end
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "friendly_id", "~> 5.1.0"
 gem "devise", "~> 4.2"
+
+gem "devcampmss_view_tool", git: "https://github.com/muhammed-salman/devcampmss_view_tool"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    DevcampmssViewTool::Renderer.copyright "Muhammed Salman", "All rights reserved"
+  end
 end
